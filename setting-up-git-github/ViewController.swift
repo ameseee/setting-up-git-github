552f29bd198b960b6b69cee3c4aa0c87c3c72069
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-03-15 17:07:30 -0600

    Initial Commit

---
+++ setting-up-git-github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  setting-up-git-github
+//
+//  Created by Holt Bolt on 3/15/20.
+//  Copyright © 2020 Amy Holt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
